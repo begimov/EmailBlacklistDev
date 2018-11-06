6a0f36a3c9277980b1efc6c4c107d3ed22c45688
begimov <begimov@gmail.com>
2018-11-06 05:26:57 +0300

    base Email model created

---
+++ packages/begimov/emailblacklist/src/Models/Email.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Begimov\Emailblacklist\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Email extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'blacklist';
+}
